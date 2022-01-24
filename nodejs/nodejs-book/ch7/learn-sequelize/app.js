bbd726a22ad748ea98b36d1301278f1ed4a71960
pksung1 <pksung1@gmail.com>
2022-01-25 04:02:37 +0900

    feat : mysql M1에서 안되서 docker 생성

--- nodejs/nodejs-book/ch7/learn-sequelize/app.js
+++ nodejs/nodejs-book/ch7/learn-sequelize/app.js
@@ -6,8 +6,8 @@ const nunjucks = require('nunjucks')
 const {sequelize} = require('./models')
 
 const indexRouter = require('./routes')
-// const usersRouter = require('./routes/users')
-// const commentsRouter = require('./routes/comments')
+const usersRouter = require('./routes/users')
+const commentsRouter = require('./routes/comments')
 
 
 const app = express();
@@ -29,8 +29,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/', indexRouter)
-// app.use('/users', usersRouter)
-// app.use('/comments', commentsRouter)
+app.use('/users', usersRouter)
+app.use('/comments', commentsRouter)
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
